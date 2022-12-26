f6f2dadf844e837669fc010deb44acd90464ddc5
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-26 13:08:53 +0300

    create card grid

--- src/App.js
+++ src/App.js
@@ -34,7 +34,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt="card front" />
+              <img className='back' src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
         ))}
     </div>
 
