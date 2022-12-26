e52b1983a6b7413cd74e9254e4f2aa912224e95b
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-26 12:52:20 +0300

    shufflings cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+
+const cardImages = [
+  {"src":"/img/helmet-1.png"},
+  {"src":"/img/potion-1.png"},
+  {"src":"/img/ring-1.png"},
+  {"src":"/img/scroll-1.png"},
+  {"src":"/img/shield-1.png"},
+  {"src":"/img/sword-1.png"},
+];
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+    
+    setCards(shuffledCards)
+    setTurns(0)
+    
+  }
+  
+  console.log(cards,turns);
+
   return (
     <div className="App">
-     test
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          
+        ))}
+    </div>
+
     </div>
   );
 }
