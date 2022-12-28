9f9d719649ed5e277159594821b800702f185d66
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-28 20:53:25 +0300

    Update SingleCard.js

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./SingleCard.css";
 
-const SingleCard = ({ card,handleChoice,flipped,disabled }) => {
-
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
   const handleClick = () => {
     if (!disabled) {
-      handleChoice(card)
-  }
-}
+      handleChoice(card);
+    }
+  };
 
   return (
     <div className="card">
-      <div className={flipped?"flipped":""}>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img
           onClick={handleClick}
